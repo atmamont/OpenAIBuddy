44785ac94e9b56571a86a39aef5dcb03a4210031
Andrii Mamchenko <atmamont@gmail.com>
2023-07-28 23:25:08 +0200

    Extract security token into xcconfig file

--- AssistantAI/Network/OpenAIHttpService.swift
+++ AssistantAI/Network/OpenAIHttpService.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 class OpenAIHttpService: HTTPService {
-    private let token = "***REMOVED***"
+    private var token: String = {
+        guard let infoDictionary: [String: Any] = Bundle.main.infoDictionary else { return "" }
+        guard let token: String = infoDictionary["OPENAI_TOKEN"] as? String else { return "" }
+        return token
+    }()
     private let url = URL(string: "https://api.openai.com/v1")!
     
     init() {
